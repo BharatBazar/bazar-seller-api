7bc1979e5b64992a0c835f4dc84625e218bc3900
roneyyb <bothra.rajat08@gmail.com>
2021-03-12 15:27:22 +0530

    Created responseHandler.ts

---
+++ src/utils/commonInterface.ts
@@ -0,0 +1,12 @@
+export interface IErrorResponse {
+    status: number;
+    statusCode: number;
+    error?: string;
+    message: string;
+    description?: string;
+    payload?: any;
+}
+
+export interface ICacheOptions {
+    key?: any;
+}
