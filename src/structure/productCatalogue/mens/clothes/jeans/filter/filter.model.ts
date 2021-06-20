4ee6ea11a3a781a3118cb2f6435efc930b271eac
roneyyb <bothra.rajat08@gmail.com>
2021-06-20 15:12:35 +0530

    Filter data for category structuring

--- src/structure/productCatalogue/mens/clothes/jeans/filter/filter.model.ts
+++ src/structure/productCatalogue/mens/clothes/jeans/filter/filter.model.ts
@@ -1,13 +1,40 @@
+import { filterType } from './../../../footwear/filter/filter/size/size.interface';
+import { IFilter } from '../../../footwear/filter/filter.interface';
+
 class JeansFilterModel {
     public async getJeansFilter() {
-        return [
+        const categoryDistibution = ['Colors', 'Size'];
+        const distribution: Partial<IFilter>[] = new Array(categoryDistibution.length);
+        let filters: IFilter[] = [];
+
+        [
             {
                 name: 'Size',
                 value: ['28', '30'],
+                filterType: 'DropDown',
                 unit: 'cm',
                 description: 'Select size in the filter',
             },
-        ];
+            {
+                name: 'Colors',
+                value: ['28', '30'],
+                filterType: 'DropDown',
+                unit: 'cm',
+                description: 'Select size in the filter',
+            },
+        ].forEach((item) => {
+            let index = categoryDistibution.indexOf(item.name);
+            if (index > -1) {
+                distribution[index] = item;
+            } else {
+                filters.push(item);
+            }
+        });
+
+        return {
+            filters,
+            distribution,
+        };
     }
 }
 
