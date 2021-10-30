45a6ca6569f28c724e36220f93a9bc63911abf9a
roneyyb <bothra.rajat08@gmail.com>
2021-10-30 23:40:59 +0530

    Minor fixes

--- src/structure/catalogue/mens/clothes/jeans/product/product/product.schema.ts
+++ src/structure/catalogue/mens/clothes/jeans/product/product/product.schema.ts
@@ -7,7 +7,7 @@ import JeansColorModel from '../colors/color.model';
 
 const JeansSchema: Schema = new Schema(
     {
-        shopId: { type: String, ref: 'Shop' },
+        shopId: { type: Types.ObjectId, ref: 'Shop' },
         title: { type: String, default: '' },
         subTitle: { type: String, default: '' },
         description: { type: String, default: '' },
