0429b14a4ac8f41bade416d2abf1d2b4fa417f92
joeyjoy007 <garvitjain5558@gmail.com>
2022-10-22 19:35:37 +0530

    changed local database

--- src/config/index.ts
+++ src/config/index.ts
@@ -21,7 +21,7 @@ export const mongoUrl = (): string => {
     };
 
     if (configs.dbAccess === 'local') {
-        return `mongodb+srv://bazarUser:${configs.pass}@cluster0.ky3gnap.mongodb.net/?retryWrites=true&w=majority`;
+        return `mongodb+srv://${configs.user}:${configs.pass}@${configs.cluster}.mongodb.net/?retryWrites=true&w=majority`;
     }
 
     let url = `mongodb+srv://${configs.user}:${configs.pass}@${configs.cluster}.mongodb.net/${configs.db}?retryWrites=true`;
