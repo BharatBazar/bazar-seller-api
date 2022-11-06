5f7deb706b727736c56e101af5ea5e91001a828d
joeyjoy007 <garvitjain5558@gmail.com>
2022-11-06 16:26:26 +0530

    fixed update api

--- src/structure/billdesk/billdesk.model.ts
+++ src/structure/billdesk/billdesk.model.ts
@@ -6,45 +6,9 @@ import { Request } from 'express';
 interface Idata {
     quantity: Number;
     price: Number;
+    itemId: String;
 }
 class BillModel {
-    // public createBill = async (data: IBill) => {
-    //     try {
-    //         const shopId = data.shopId;
-    //         const exist = await Bill.find({ shopId });
-    //         // console.log(
-    //         //     'EXIST',
-    //         //     exist[0].products.map((e) => {
-    //         //         return console.log('IDS', e.productSize === data.products[0].productSize);
-    //         //     }),
-    //         // );
-    //         if (exist[0]) {
-    //             const firstCheck = exist[0].products.map((e) => {
-    //                 return String(e.productSize);
-    //             });
-
-    //             const secondCheck = data.products.map((e) => {
-    //                 return e.productSize;
-    //             });
-
-    //             const mapCheck = firstCheck.map((e) => {
-    //                 return e === secondCheck[0];
-    //             });
-
-    //             console.log('FIRST_CHECK', firstCheck);
-    //             console.log('SECOND_CHECK', secondCheck);
-    //             console.log('MAP_CHECK', mapCheck);
-    //             return firstCheck;
-    //             // const getConfirmation = exist[0].products;
-    //             // console.log('CONFIRM', getConfirmation);
-    //         } else {
-    //             const bill = new Bill(data);
-    //             return await bill.save();
-    //         }
-    //     } catch (error) {
-    //         throw new HTTP400Error('Bill not created', error.message);
-    //     }
-    // };
     public createBill = async (data: IBill) => {
         try {
             const bill = new Bill(data);
@@ -91,16 +55,18 @@ class BillModel {
     };
     public updateBill = async (req: Request, data: Idata) => {
         const _id = req.params.id;
-        console.log('OP', _id);
-        console.log('DATA', data);
+        const { quantity, price, itemId } = data;
 
         try {
-            const bill = await Bill.findById(_id);
-            var update = { quantity: data.quantity, price: data.prices, productSize: data.itemId };
-            const updateBill = await Bill.updateOne(
-                { 'products.productSize': data.itemId },
-                { $set: { 'products.$': update } },
+            var update = { quantity, price, productSize: itemId };
+            const updateBill = await Bill.findOneAndUpdate(
+                {
+                    _id: _id,
+                    'products.productSize': data.itemId,
+                },
+                { $set: { 'products.$': update, totalPrice: Number(quantity) * Number(price) } },
             );
+            console.log('UP', updateBill);
             return updateBill;
         } catch (error: any) {
             throw new HTTP400Error('Bill not updated', error.message);
