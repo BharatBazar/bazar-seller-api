e007983047acc41c4eac344c4f847ae3113cc11c
roneyyb <bothra.rajat08@gmail.com>
2022-11-16 11:17:42 +0530

    Added comment in code for better understanding

--- src/structure/catalogue/catalogue/productCatalogue.model.ts
+++ src/structure/catalogue/catalogue/productCatalogue.model.ts
@@ -3,119 +3,120 @@ import { HTTP400Error } from '../../../lib/utils/httpErrors';
 import { categoryType, IProductCatalogue, IProductCatalogueModel } from './productCatalogue.interface';
 import { ProductCatalogue } from './productCatalogue.schema';
 class ProductCatalogueModel {
-    
-    public async CatalogueExistOrNot(_id:string) {
-      return await ProductCatalogue.findById(_id);
+    public async CatalogueExistOrNot(_id: string) {
+        return await ProductCatalogue.findById(_id);
     }
 
-
     public async AddProductCatalogue(data: IProductCatalogue) {
-        console.log("PPOOO",data);
-        if(!data.type) {
-            throw new HTTP400Error("Please provide unique type for category you are adding");
-        } else
-        if (await ProductCatalogue.findOne({ type:data.type })) {
+        if (!data.type) {
+            throw new HTTP400Error('Please provide unique type for category you are adding');
+        } else if (await ProductCatalogue.findOne({ type: data.type })) {
             throw new HTTP400Error('Product type already exist.');
         } else {
             const category: IProductCatalogueModel = new ProductCatalogue(data);
             // await category.save()
             if (data.parent) {
-                    console.log("DP",data.parent)
-                    const parent = await ProductCatalogue.findByIdAndUpdate(data.parent, {
+                const parent = await ProductCatalogue.findByIdAndUpdate(
+                    data.parent,
+                    {
                         $push: { child: category._id },
-                    },{new:true});
-                    console.log("parent",parent)
-                    if (!parent) {
-                        throw new HTTP400Error('Parent not found');
-                    } else {
-                        let parentPath = parent.path;
-                        let parentId: Types.ObjectId = parent._id;
+                    },
+                    { new: true },
+                );
+                console.log('parent', parent);
+                if (!parent) {
+                    throw new HTTP400Error('Parent not found');
+                } else {
+                    let parentPath = parent.path;
+                    let parentId: Types.ObjectId = parent._id;
 
-                        let childPath = [...parentPath, parentId];
-                        category.path = [...childPath];
-                    }
-                } 
+                    let childPath = [...parentPath, parentId, category._id];
+                    category.path = [...childPath];
+                }
+            }
             await category.save();
             return category;
         }
     }
 
     public async DeleteProductCatalogue(data: IProductCatalogueModel) {
-        const exist: IProductCatalogueModel | null = await ProductCatalogue.findById(data._id);
-        if (exist) {
-            if (exist.categoryType == categoryType.Category) {
-                const allItem = await ProductCatalogue.find({ parent: exist._id });
-                let deletePromises = allItem.map(
-                    (item) =>
-                        new Promise(async (resolve) => {
-                            resolve(await ProductCatalogue.deleteMany({ parent: item._id }));
-                        }),
-                );
-                await Promise.all([...deletePromises, await ProductCatalogue.findByIdAndDelete(exist._id)]);
+        if (data._id) {
+            const exist: IProductCatalogueModel | null = await ProductCatalogue.findById(data._id);
+            if (exist) {
+                if (exist.type == categoryType.Category) {
+                    const allItem = await ProductCatalogue.find({ parent: exist._id });
+                    let deletePromises = allItem.map(
+                        (item) =>
+                            new Promise(async (resolve) => {
+                                resolve(await ProductCatalogue.deleteMany({ parent: item._id }));
+                            }),
+                    );
+                    await Promise.all([...deletePromises, await ProductCatalogue.findByIdAndDelete(exist._id)]);
 
-                return 'Deleted';
-            } else if (exist.categoryType == categoryType.SubCategory) {
-                await Promise.all([
-                    await ProductCatalogue.deleteMany({ parent: exist._id }),
-                    await ProductCatalogue.findByIdAndUpdate(exist.parent, { $pull: { child: exist._id } }),
-                    await ProductCatalogue.findByIdAndDelete(exist._id),
-                ]);
-                return 'Deleted';
-            } else if (exist.categoryType == categoryType.SubCategory1) {
-                await Promise.all([
-                    await ProductCatalogue.findByIdAndUpdate(exist.parent, { $pull: { child: exist._id } }),
-                    await ProductCatalogue.findByIdAndDelete(exist._id),
-                ]);
-                return 'Deleted';
+                    return 'Deleted';
+                } else if (exist.type == categoryType.SubCategory) {
+                    await Promise.all([
+                        await ProductCatalogue.deleteMany({ parent: exist._id }),
+                        await ProductCatalogue.findByIdAndUpdate(exist.parent, { $pull: { child: exist._id } }),
+                        await ProductCatalogue.findByIdAndDelete(exist._id),
+                    ]);
+                    return 'Deleted';
+                } else if (exist.type == categoryType.SubCategory1) {
+                    await Promise.all([
+                        await ProductCatalogue.findByIdAndUpdate(exist.parent, { $pull: { child: exist._id } }),
+                        await ProductCatalogue.findByIdAndDelete(exist._id),
+                    ]);
+                    return 'Deleted';
+                }
+            } else {
+                throw new HTTP400Error('Category deleted!1');
             }
         } else {
-            throw new HTTP400Error('Category deleted!1');
+            throw new HTTP400Error('Catalogue id not provided');
         }
     }
 
-    public async GetProductCatalogue(query: IProductCatalogue) {
-        console.log('query => ', query);
-        const data = await ProductCatalogue.find(query)
-        .populate
-        ({ path: 'parent child',
-           select: 'name child image description' ,
-           
-              populate: {
-                path: 'child',
-                select: 'name image description',
+    public async GetProductCatalogue(param: IProductCatalogue) {
+        let query = typeof param != 'object' ? {} : param;
+        console.log('Query', query, param);
+        const data = await ProductCatalogue.find(query).populate({
+            path: 'parent child',
+
+            populate: {
+                path: 'child parent',
             },
-    
-    });
-    console.log("GET_DATA",data)
+        });
+
+        console.log('DATA', query, data.length);
+
         return data;
     }
 
     public async GetProductCatalogueWithAncestors(quer: IProductCatalogue) {
         const a = new Promise(async (resolve) => {
-            
-        resolve(await ProductCatalogue.find({
-           // child:undefined,
-           child:[],
-            
-            active:true
-        })
-        .populate({
-            path: 'parent',
-            select: 'name',
-            
-            populate: {
-                path: 'parent',
-                select: 'name',
-            },
-        })
-    });
+            resolve(
+                await ProductCatalogue.find({
+                    // child:undefined,
+                    child: [],
 
-   
+                    active: true,
+                }).populate({
+                    path: 'parent',
+                    select: 'name',
 
-    const data =  await Promise.all([await a]);
-    console.log(data);
-    return [].concat.apply([],data);
+                    populate: {
+                        path: 'parent',
+                        select: 'name',
+                    },
+                }),
+            );
+        });
+
+        const data = await Promise.all([await a]);
+        console.log(data);
+        return [].concat.apply([], data);
     }
+
     public async UpdateProductCatalogue(data: IProductCatalogue) {
         if ('activate' in data) {
             throw new HTTP400Error('Cannot activate catelogue item from this api.');
@@ -128,36 +129,44 @@ class ProductCatalogueModel {
         }
     }
 
-    private async checkAndActicateCatalogueItem(exist: IProductCatalogueModel) {
-        const categoryList: IProductCatalogueModel | null = await ProductCatalogue.findById(exist._id).populate({
-            path: 'child',
-            select: 'active',
-        });
+    private async checkAndActivateCatalogueItem(exist: IProductCatalogueModel) {
+        if (exist._id) {
+            const categoryList: IProductCatalogueModel | null = await ProductCatalogue.findById(exist._id).populate({
+                path: 'child',
+                select: 'active',
+            });
 
-        if (categoryList && exist.subCategoryExist && exist.child.length > 0) {
-            let flag = categoryList.child.some((child) => child.active);
-            if (flag) {
+            if (categoryList && exist.child.length > 0) {
+                let flag = categoryList.child.some((child) => child.active);
+                if (flag) {
+                    await ProductCatalogue.findByIdAndUpdate(exist._id, { active: !exist.active });
+                    return 'Catalogue item activated';
+                } else {
+                    throw new HTTP400Error('Catalogue does not have any active child and child exist.');
+                }
+            } else if (!exist.subCategoryExist) {
                 await ProductCatalogue.findByIdAndUpdate(exist._id, { active: !exist.active });
                 return 'Catalogue item activated';
             } else {
-                throw new HTTP400Error('Catalogue does not have any active child and child exist.');
+                throw new HTTP400Error(
+                    'Catalogoue item cannot be activated since no child has been added but child exist.',
+                );
             }
-        } else if (!exist.subCategoryExist) {
-            await ProductCatalogue.findByIdAndUpdate(exist._id, { active: !exist.active });
-            return 'Catalogue item activated';
         } else {
-            throw new HTTP400Error(
-                'Catalogoue item cannot be activated since no child has been added but child exist.',
-            );
+            throw new HTTP400Error('Provide catalogue id');
         }
     }
 
     public async ActivateCatalogue(data: { _id: Types.ObjectId; activate: boolean }) {
-        const exist = await ProductCatalogue.findById(data._id);
-        if (exist) {
-            return await this.checkAndActicateCatalogueItem(exist);
+        if (data._id) {
+            const exist = await ProductCatalogue.findById(data._id);
+            if (exist) {
+                return await this.checkAndActivateCatalogueItem(exist);
+            } else {
+                throw new HTTP400Error('Catalogue item does not exist please add product in order to update it.');
+            }
         } else {
-            throw new HTTP400Error('Catalogue item does not exist please add product in order to update it.');
+            throw new HTTP400Error('Provide catalogue id');
         }
     }
 }
--- src/structure/catalogue/filter/filter/filter.interface.ts
+++ src/structure/catalogue/filter/filter/filter.interface.ts
@@ -6,25 +6,67 @@ export interface IFilter {
     name: string; //Filter name Like waist size
     description: string; // Filter details descipbing about filter
     image: string; // Image url
-    type: filterValuesTypes; // It will refer to the type to which the filter belongs
-    multiple: boolean; // Multiple values can be selected or not
-    //filterLevel: number; // 0 means filter only and 1 means It is top level distribution like color 2 means inside distibution that is size or etc.
-    active: boolean; // It is used to active a filter and show it publically so that filter can through a verifying flow and all good then they are release to public
 
-    /*
-    filterActivatedCount: This is added to solve problem of incrementing totalFilterAdded count by 1
-    it should only happen when filter is activated for the first time
-    */
-    filterActivatedCount: number;
-    mandatory: boolean;
     customerHeading: string;
     customerDescription: string;
     customerImage: string;
-    defaultSelectAll: boolean;
-    parent: ObjectId;
-    showSearch: boolean;
-    key: string;
+
+    //type: filterValuesTypes; // It will refer to the type to which the filter belongs
+
+    /*
+     * IF MULTIPLE VALUE CAN BE SELECTED FOR THIS FITLER FOR A PRODUCT
+     */
+    multiple: boolean;
+
+    /*
+     * Basically this tells if filter is a main category or not like color
+     * or like other filter can rendered normally
+     * 0 means filter only
+     * 1 means It is top level distribution like color
+     * 2 means inside distibution that is size or etc.
+     */
     filterLevel: number;
+
+    /*
+     * It is used to active a filter and show it publically so
+     * that filter can through a verifying flow and all good then
+     * they are release to public
+     */
+    active: boolean;
+
+    /*
+    filterActivatedCount: This is added to solve problem of incrementing totalFilterAdded count by 1
+    it should only happen when filter is activated for the first time as filter can be activated or deactivated 
+    several times
+    */
+    filterActivatedCount: number;
+
+    /*
+     * THIS TELLS THAT FILTER NEED TO BE PROVIDED WHILE PRODUCT LISTING
+     */
+    mandatory: boolean;
+
+    /*
+     * IF THIS IS TRUE THEN ALL THE FILTER VALUE FOR THIS FILTER WILL BE
+     * DEFAULT SELECTED WHEN SELLER IS PROVIDING FILTER FOR HIS SHOP
+     */
+    defaultSelectAll: boolean;
+
+    /*
+     * PARENT IS BASICALLY CATALOGUE TO WHICH THIS FILTER BELONGS
+     */
+    parent: ObjectId;
+
+    /*
+     * IF THIS IS SET AS TRUE THEN A SEARCH WILL OPEN WHEN SELLER IS PROVIDING
+     * FILTER VALUE FOR HIS SHOP FOR THIS PARTICULAR FILTER
+     */
+    showSearch: boolean;
+
+    /*
+     * THIS IS UNIQUE IDENTIFICATION FOR EVERY FILTER
+     */
+    key: string;
 }
 
 export interface IFilterModel extends Document, IFilter {}
